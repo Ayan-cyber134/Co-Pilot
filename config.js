525c03949dd58b0aa9582b92d22fd77be66da372
Ayan-cyber134 <155625564+Ayan-cyber134@users.noreply.github.com>
2024-09-22 14:44:33 +0500

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ module.exports = {
 
 	//	These values will be ignored if you have set the environment variables (must be in uppercase)
 
-	prefix: '!', // required, command prefix
-	botToken: '', // required, https://discordjs.guide/preparations/setting-up-a-bot-application.html#your-token
-	clientId: '', // optional, the bot's client ID, leave empty to disable slash commands
+	prefix: ':', // required, command prefix
+	botToken: 'OTM2NjgyODQ5MDY5ODQyNDMz.G5U-Sb.1nQX8UZMQVu79GYz9MPFMsupBq_ZwS7IM7r9DI', // required, https://discordjs.guide/preparations/setting-up-a-bot-application.html#your-token
+	clientId: '936682849069842433', // optional, the bot's client ID, leave empty to disable slash commands
 	geniusApiToken: '', // optional, but recommended for lyrics search - https://genius.com/api-clients
 
 	// still under development, so you should leave these empty
