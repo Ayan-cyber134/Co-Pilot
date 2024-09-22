06449dc50d17f00057016711b53eab53448e823b
Ayan-cyber134 <155625564+Ayan-cyber134@users.noreply.github.com>
2024-09-22 14:50:27 +0500

    Update config.js
    Signed-off-by: Ayan-cyber134 <155625564+Ayan-cyber134@users.noreply.github.com>

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
 	//	These values will be ignored if you have set the environment variables (must be in uppercase)
 
 	prefix: ':', // required, command prefix
-	botToken: 'OTM2NjgyODQ5MDY5ODQyNDMz.G5U-Sb.1nQX8UZMQVu79GYz9MPFMsupBq_ZwS7IM7r9DI', // required, https://discordjs.guide/preparations/setting-up-a-bot-application.html#your-token
+	botToken: '', // required, https://discordjs.guide/preparations/setting-up-a-bot-application.html#your-token
 	clientId: '936682849069842433', // optional, the bot's client ID, leave empty to disable slash commands
 	geniusApiToken: '', // optional, but recommended for lyrics search - https://genius.com/api-clients
 
